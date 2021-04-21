ab2a3cf105a83e0e293e0af224190e69e198d0e5
zhongqi <imzhongqi@gmail.com>
2021-04-21 11:11:37 +0800

    update alias

--- zsh/.config/zsh/snippets/alias.zsh
+++ zsh/.config/zsh/snippets/alias.zsh
@@ -2,33 +2,20 @@ function _z() {
   _zlua "$@";
 }
 
-ap() {
-  case $1 in
-    status | s)
-      if is_proxy; then
-        echo "status: on, proxy address $http_proxy"
-      else
-        echo "status: off"
-      fi
-      ;; 
-
-    on)
-      proxy "$2"
-      ;;
-
-    off)
-      unproxy
-      ;;
-
-    "")
-      (is_proxy && unproxy) || proxy
-      ;;
-
-    *)
-      echo "error: unknown arguments"
-      ;;
-  esac
+command_check() {
+	command -v $1 &>/dev/null
 }
+
+command_check exa && alias ls='exa -bh'
+command_check bat && alias cat='bat --plain'
+
+# global alias
+alias rm='rm -i'   rd='rmdir'   md='mkdir -p'
+alias la='ls -la'  lt='ls --tree'  ll='ls -l'  l='ls'
+alias dfh='df -h'  dus='du -sh' del='gio trash' dusa='dus --apparent-size'
+alias reload="exec $SHELL -l -i"  grep="command grep --colour=auto --binary-files=without-match --directories=skip"
+alias df='df -h'  du='du -h'  plast="last -20"
+alias cp='cp -v'   mv='mv -v'
 
 proxy() {
   if [[ "$1" != "" ]]; then
@@ -49,18 +36,30 @@ is_proxy() {
   fi
 }
 
-command_check() {
-	command -v $1 &>/dev/null
+ap() {
+  case $1 in
+    status | s)
+      if is_proxy; then
+        echo "status: on, proxy address $http_proxy"
+      else
+        echo "status: off"
+      fi
+      ;; 
+
+    on)
+      proxy "$2"
+      ;;
+
+    off)
+      unproxy
+      ;;
+
+    "")
+      is_proxy && unproxy || proxy
+      ;;
+
+    *)
+      echo "error: unknown arguments"
+      ;;
+  esac
 }
-
-command_check exa && alias ls='exa -bh'
-command_check bat && alias cat='bat --plain'
-
-# global alias
-alias rm='rm -i'   rd='rmdir'   md='mkdir -p'
-alias la='ls -la'  lt='ls --tree'  ll='ls -l'  l='ls'
-alias dfh='df -h'  dus='du -sh' del='gio trash' dusa='dus --apparent-size'
-alias reload="exec $SHELL -l -i"  grep="command grep --colour=auto --binary-files=without-match --directories=skip"
-alias df='df -h'  du='du -h'  plast="last -20"
-alias cp='cp -v'   mv='mv -v'
-
