52912610a6ff270789656d3fe08e2ef528550dc3
zhongqi <imzhongqi@gmail.com>
2022-03-03 16:45:38 +0800

    添加注释

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -6,6 +6,10 @@ require "plugins.lsp"
 
 require "plugins.colorscheme"
 
+require "plugins.nvim-tree"
+
+require "plugins.telescope"
+
 require "plugins.treesitter"
 
 require "plugins.comment"
@@ -14,36 +18,42 @@ require "plugins.autopairs"
 
 require "plugins.gitsigns"
 
-require "plugins.nvim-tree"
-
 require "plugins.alpha"
-
-require "plugins.telescope"
 
 require "plugins.lualine"
 
 require "plugins.whichkey"
 
+-- 上方的类似 tab 的选项卡
 require "plugins.bufferline"
 
-require "plugins.completion"
-
+-- terminal
 require "plugins.toggleterm"
 
+-- 补全
 require "plugins.completion"
 
+-- 缩进线
 require "plugins.indentline"
 
+-- 安装 debugger 
 require "plugins.nvim-dap-install"
 
+-- debugger
 require "plugins.nvim-dap"
 
+-- debug 的时候的 ui
 require "plugins.nvim-dap-ui"
 
+
+-- debug 的时候代码旁边显示的虚拟文字
 require "plugins.nvim-dap-virtual-text"
 
+-- 诊断
 require "plugins.trouble"
 
+-- 滚动条
 require "plugins.neoscroll"
 
+-- 高亮当前光标内容
 require "plugins.illuminate"
