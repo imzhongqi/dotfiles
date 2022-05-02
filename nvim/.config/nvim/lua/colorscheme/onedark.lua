6af3ec28b741648304cce4ecb221e884f34f36a2
zhongqi <imzhongqi@gmail.com>
2022-05-02 11:36:44 +0800

    update ui

--- nvim/.config/nvim/lua/colorscheme/onedark.lua
+++ nvim/.config/nvim/lua/colorscheme/onedark.lua
@@ -35,27 +35,6 @@ onedark.setup({
             -- Telescope
             TelescopeNormal = { bg = c.bg1 },
             TelescopeBorder = { fg = "#61afef", bg = c.bg1 },
-
-            -- DAP UI
-            DapUIVariable = { link = "Normal" },
-            DapUIScope = { fg = "#ffffff" },
-            DapUIType = { fg = "#60878b" },
-            DapUIValue = { link = "Normal" },
-            DapUIModifiedValue = { fg = "#00F1F5", style = "bold" },
-            DapUIDecoration = { fg = "#fcecc5" },
-            DapUIThread = { fg = "#434649" },
-            DapUIStoppedThread = { fg = "#ffffff" },
-            DapUIFrameName = { link = "Normal" },
-            DapUISource = { fg = "#106ba3" },
-            DapUILineNumber = { fg = "#949699" },
-            DapUIFloatBorder = { fg = "ffffff" },
-            DapUIWatchesEmpty = { fg = "#F70067" },
-            DapUIWatchesValue = { fg = "#A9FF68" },
-            DapUIWatchesError = { fg = "F70067" },
-            DapUIBreakpointsPath = { fg = "#ffffff" },
-            DapUIBreakpointsInfo = { fg = "#949699" },
-            DapUIBreakpointsCurrentLine = { fg = "#949699", style = "bold" },
-            DapUIBreakpointsLine = { link = "DapUILineNumber" },
         }
     end,
 })
