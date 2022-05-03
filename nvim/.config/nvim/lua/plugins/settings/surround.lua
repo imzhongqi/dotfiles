cb9972ac445c8bd270575b15e2f78e635761880b
zhongqi <imzhongqi@gmail.com>
2022-05-03 15:37:26 +0800

    update surround

--- nvim/.config/nvim/lua/plugins/settings/surround.lua
+++ nvim/.config/nvim/lua/plugins/settings/surround.lua
@@ -1,7 +1,12 @@
-require("surround").setup({
+local status_ok, surround = pcall(require, "surround")
+if not status_ok then
+    return
+end
+
+surround.setup({
     context_offset = 100,
     load_autogroups = false,
-    mappings_style = "sandwich",
+    mappings_style = "surround",
     map_insert_mode = true,
     quotes = { "'", '"' },
     brackets = { "(", "{", "[" },
