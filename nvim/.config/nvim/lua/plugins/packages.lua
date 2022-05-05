202ac0eeec9bedb44cf9591eb02737229cc49a6e
zhongqi <imzhongqi@gmail.com>
2022-05-05 23:20:13 +0800

    remove pretty-fold.nvim plugin

--- nvim/.config/nvim/lua/plugins/packages.lua
+++ nvim/.config/nvim/lua/plugins/packages.lua
@@ -108,34 +108,4 @@ return {
 
     -- { "f3fora/cmp-spell" },
     { "ur4ltz/surround.nvim" },
-
-    {
-        "anuvyklack/pretty-fold.nvim",
-        requires = "anuvyklack/nvim-keymap-amend", -- only for preview
-        config = function()
-            require("pretty-fold").setup({
-                keep_indentation = false,
-                fill_char = "━",
-                sections = {
-                    left = {
-                        "━ ",
-                        function()
-                            return string.rep("*", vim.v.foldlevel)
-                        end,
-                        " ━┫",
-                        "content",
-                        "┣",
-                    },
-                    -- right = {
-                    --     "┫ ",
-                    --     "number_of_folded_lines",
-                    --     ": ",
-                    --     "percentage",
-                    --     " ┣━━",
-                    -- },
-                },
-            })
-            require("pretty-fold.preview").setup()
-        end,
-    },
 }
