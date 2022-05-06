cc221892cc0355f8961e3dac904889fd429ffb4d
zhongqi <imzhongqi@gmail.com>
2022-05-06 09:58:54 +0800

    update plugin config

--- nvim/.config/nvim/lua/plugins/settings/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/settings/gitsigns.lua
@@ -3,4 +3,13 @@ if not status_ok then
     return
 end
 
-gitsigns.setup({})
+gitsigns.setup({
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 500,
+        ignore_whitespace = false,
+    },
+    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> • <summary>",
+})
