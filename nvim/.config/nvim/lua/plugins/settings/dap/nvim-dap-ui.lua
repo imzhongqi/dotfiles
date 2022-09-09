a24107c6d852df5c3435719dac331349a9d7f1b6
azsky <imzhongqi@gmail.com>
2022-09-09 10:15:08 +0800

    update nvim

--- nvim/.config/nvim/lua/plugins/settings/dap/nvim-dap-ui.lua
+++ nvim/.config/nvim/lua/plugins/settings/dap/nvim-dap-ui.lua
@@ -14,30 +14,6 @@ dapui.setup({
         repl = "r",
     },
 
-    sidebar = {
-        -- You can change the order of elements in the sidebar
-        elements = {
-            -- Provide as ID strings or tables with "id" and "size" keys
-            {
-                id = "scopes",
-                size = 0.4, -- Can be float or integer > 1
-            },
-            { id = "breakpoints", size = 0.2 },
-            { id = "stacks", size = 0.1 },
-        },
-        size = 40,
-        position = "left", -- Can be "left", "right", "top", "bottom"
-    },
-
-    tray = {
-        elements = {
-            { id = "repl", size = 1 },
-            -- { id = "watches", size = 0.2 },
-        },
-        size = 10,
-        position = "bottom", -- Can be "left", "right", "top", "bottom"
-    },
-
     floating = {
         max_height = 0.5, -- These can be integers or a float between 0 and 1.
         max_width = 0.5, -- Floats will be treated as percentage of your screen.
